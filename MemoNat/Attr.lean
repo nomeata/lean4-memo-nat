cda677dfb2c8d06fc064d450200027f5530982e2
Joachim Breitner <mail@joachim-breitner.de>
2023-10-22 16:14:41 +0200

    Port fixes from master

--- MemoNat/Attr.lean
+++ MemoNat/Attr.lean
@@ -7,16 +7,14 @@ def memoAttrImpl (n : Name) : AttrM Unit := do
   let defn ← getConstInfoDefn n
 
   let (u, f, drt, dmemo, eq_dmemo) <- match defn.value with
-    | .app (.app (.app (.app (.app (.const ``WellFounded.fix [_u, u]) dt) drt) _) _) f => do
+    | .app (.app (.app (.app (.app (.const ``WellFounded.fix [_u, u]) dt) drt) _) _) f =>
       match dt with
-      | .const `Nat _ => pure ()
-      | _ =>  throwError ("first argument to fix not `Nat` but" ++ toString dt)
-
-
+      | .const ``Nat _ => pure ()
+      | _ =>  throwError s!"first argument to fix not `Nat` but {dt}"
       pure (u, f, drt, ``NatMemo.dmemo, ``NatMemo.fix_eq_dmemo)
-    | .lam _ (.const `Nat _) ( .app (.app (.app (.const `Nat.brecOn [u]) drt) _) f) _ => do
+    | .lam _ (.const ``Nat _) ( .app (.app (.app (.const ``Nat.brecOn [u]) drt) _) f) _ =>
       pure (u, f, drt, ``NatMemo.dmemo_below, ``NatMemo.brecOn_eq_dmemo_below)
-    | _ => throwError ("definition right hand side not of expected form: " ++ toString defn.value)
+    | _ => throwError s!"definition right hand side not of expected form: {defn.value}"
     
   let slow_name := defn.name
   let fast_name := slow_name ++ `fast
@@ -25,13 +23,14 @@ def memoAttrImpl (n : Name) : AttrM Unit := do
   addAndCompile (.defnDecl { defn with
     name := fast_name
     -- NB: This declaration has the same type as slow
-    value := mkAppN (mkConst dmemo [u]) #[drt, f]
+    value := mkAppN (.const dmemo [u]) #[drt, f]
   })
 
-  addAndCompile (.thmDecl { defn with
+  addDecl (.thmDecl { defn with
     name := eq_name
-    type := mkAppN (mkConst `Eq [u]) #[defn.type, mkConst slow_name, mkConst fast_name]
-    value := mkAppN (mkConst eq_dmemo [u]) #[drt, f]
+    type := mkAppN (.const ``Eq [u]) #[defn.type, .const slow_name [], .const fast_name []]
+     -- FIXME: is it necessarily the case that `slow_name` and `fast_name` have no universe params?
+    value := mkAppN (.const eq_dmemo [u]) #[drt, f]
   })
 
   Lean.Compiler.CSimp.add eq_name AttributeKind.global
